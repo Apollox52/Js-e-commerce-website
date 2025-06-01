8933f58e0b390581eaaca63c8d0a9572b8fbb1a2
mouad <mr.robott52@gmail.com>
2025-06-01 13:41:07 +0100

    cards , first responsiveness setup

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//navbar
+const sidebar = document.getElementsByClassName('sidebar')[0];
+function showSidebar() {
+   sidebar.style.display = 'flex';  
+}
+function hideSidebar(){
+  sidebar.style.display = 'none';
+}
