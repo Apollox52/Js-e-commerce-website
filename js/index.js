74099678212acf0ec93f58185abca023d8176c44
mouad <mr.robott52@gmail.com>
2025-06-03 23:21:41 +0100

    animations and new sections added

--- js/index.js
+++ js/index.js
@@ -6,3 +6,46 @@ function showSidebar() {
 function hideSidebar(){
   sidebar.style.display = 'none';
 }
+
+//Right To left animation
+
+const cards = document.querySelectorAll('.card1');
+const cards2 = document.querySelectorAll('.card2');
+const images = document.querySelectorAll('.img');
+
+const observer = new IntersectionObserver ((entries)=>{
+  entries.forEach((entry,index)=>{
+    if(entry.isIntersecting){
+      setTimeout(() => {
+        entry.target.classList.add('visible');
+      }, index * 200);
+
+      observer.unobserve(entry.target);
+    }
+  });
+},{
+  threshold:0.4
+});
+
+
+cards.forEach(card=>{
+  observer.observe(card);
+});
+
+images.forEach(image=>{
+  observer.observe(image);
+});
+
+cards2.forEach((card)=>{
+  observer.observe(card);
+})
+
+
+
+
+
+
+
+
+
+
